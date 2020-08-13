fa3c4750e37ed6161b35cbdaf6ce9503b9dfaacc
gongsi01 <pengpengker9@gmail.com>
2020-08-13 18:42:09 +0800

    终结

---
+++ leida/navjs.js
@@ -0,0 +1,1 @@
+function slide(){var e=document.getElementById("navBox"),n=document.getElementById("menuBox");"none"==e.style.display?(e.style.display="block",n.style.left="330px"):(e.style.display="none",n.style.left="0px")}document.getElementById("menuBox").onclick=function(){slide()};
